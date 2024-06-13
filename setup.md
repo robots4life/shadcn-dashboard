e162b297c254761e06e526967a0497b65fe72df3
robots4life <34193243+robots4life@users.noreply.github.com>
2024-06-13 19:05:15 +0200

    use Froms example

--- setup.md
+++ setup.md
@@ -44,3 +44,41 @@ px shadcn-svelte@latest add button
 <a href="http://localhost:5173/002">002</a>
 
 For <a href="http://localhost:5173/002">002</a> use <a href="https://www.shadcn-svelte.com/examples/forms">https://www.shadcn-svelte.com/examples/forms</a> with this code <a href="https://github.com/huntabyte/shadcn-svelte/tree/main/apps/www/src/routes/(app)/examples/forms">https://github.com/huntabyte/shadcn-svelte/tree/main/apps/www/src/routes/ app)/examples/forms</a> as an example.
+
+```shell
+px shadcn-svelte@latest add separator
+```
+
+https://zod.dev/?id=from-npm-nodebun
+
+```shell
+pa zod
+```
+
+https://superforms.rocks/get-started
+
+```shell
+pi -D sveltekit-superforms
+```
+
+https://www.shadcn-svelte.com/docs/components/form
+
+```shell
+px shadcn-svelte@latest add form
+```
+
+```shell
+px shadcn-svelte@latest add select
+```
+
+```shell
+px shadcn-svelte@latest add input
+```
+
+```shell
+px shadcn-svelte@latest add textarea
+```
+
+```shell
+px shadcn-svelte@latest add checkbox
+```
---
+++ src/routes/002/+layout.svelte
@@ -0,0 +1,49 @@
+<script lang="ts">
+	import SidebarNav from './(components)/sidebar-nav.svelte';
+	import { Separator } from '$lib/components/ui/separator/index.js';
+	// import FormsLight from '$lib/img/examples/forms-light.png?enhanced';
+	// import FormsDark from '$lib/img/examples/forms-dark.png?enhanced';
+
+	const sidebarNavItems = [
+		{
+			title: 'Profile',
+			href: '/002'
+		},
+		// {
+		// 	title: 'Account',
+		// 	href: '/002/account'
+		// }
+		// {
+		// 	title: 'Appearance',
+		// 	href: '/002/appearance'
+		// },
+		// {
+		// 	title: 'Notifications',
+		// 	href: '/002/notifications'
+		// },
+		{
+			title: 'Display',
+			href: '/002/display'
+		}
+	];
+</script>
+
+<!-- <div class="md:hidden">
+	<enhanced:img src={FormsLight} alt="Forms" class="block dark:hidden" />
+	<enhanced:img src={FormsDark} alt="Forms" class="hidden dark:block" />
+</div> -->
+<div class="hidden space-y-6 p-10 pb-16 md:block">
+	<div class="space-y-0.5">
+		<h2 class="text-2xl font-bold tracking-tight">Settings</h2>
+		<p class="text-muted-foreground">Manage your account settings and set e-mail preferences.</p>
+	</div>
+	<Separator class="my-6" />
+	<div class="flex flex-col space-y-8 lg:flex-row lg:space-x-12 lg:space-y-0">
+		<aside class="-mx-4 lg:w-1/5">
+			<SidebarNav items={sidebarNavItems} />
+		</aside>
+		<div class="flex-1 lg:max-w-2xl">
+			<slot />
+		</div>
+	</div>
+</div>
